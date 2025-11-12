64cb27215945d377753101966ce2b844a065a9db
Bruno Rauber <brunorauber.dev@gmail.com>
2025-11-12 20:44:49 -0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,7 +7,7 @@
  */
 function convertToObject(sourceString) {
   const lines = sourceString
-    .split('\n')
+    .split(/'\n'|;/)
     .map((line) => line.trim())
     .filter((line) => line !== '' && line.indexOf(':') !== -1 && line !== ';');
 
@@ -32,6 +32,40 @@ function convertToObject(sourceString) {
   return style;
 }
 
-convertToObject();
+convertToObject(`
+  background-color:      #fff;
+-webkit-border-radius: 5px;
+  border-radius     : 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear   : both       ;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+      font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height:    40px;
+  outline: 0;
+  padding-left    : 18px;
+  padding-right: 30px;
+  ;
+
+  ;
+  position: relative;
+
+
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+
+  white-space: nowrap;
+  width: auto;
+`);
 
 module.exports = convertToObject;
