9aa53bc5fcb9dcb3a9360f991dae09e7e5ef0d69
Bruno Rauber <brunorauber.dev@gmail.com>
2025-11-12 18:03:26 -0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,65 +6,32 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = sourceString
-    .replace(/\s*\n\s*/g, ' ')
-    .split(/;|\n/)
+  const lines = sourceString
+    .split('\n')
     .map((line) => line.trim())
-    .filter((item) => item !== '' && item !== ';');
+    .filter((line) => line !== '' && line.indexOf(':') !== -1 && line !== ';');
 
-  const indexToPoints = result.map((item) => item.indexOf(':'));
+  const style = {};
 
-  const resultObj = {};
+  lines.forEach((line) => {
+    const i = line.indexOf(':');
 
-  for (let i = 0; i < result.length; i++) {
-    const key = result[i].slice(0, indexToPoints[i]);
-    let value = result[i].slice(indexToPoints[i] + 1);
-
-    if (value[value.length - 1] === ';') {
-      value = value.slice(0, -1).trim();
+    if (i === -1) {
+      return;
     }
 
-    resultObj[key.trim()] = value.trim();
-  }
-  console.log(resultObj);
+    const key = line.slice(0, i).trim();
+    const value = line
+      .slice(i + 1)
+      .replace(/;$/, '')
+      .trim();
 
-  return resultObj;
+    style[key] = value;
+  });
+
+  return style;
 }
 
-convertToObject(`
-  background-color:      #fff;
--webkit-border-radius: 5px;
-  border-radius     : 5px;
-  border: 1px solid #e8e8e8;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  clear   : both       ;
-  cursor: pointer;
-  float: left;
-  font-family: inherit;
-      font-size: 14px;
-  font-weight: 400;
-  height: 42px;
-  line-height:    40px;
-  outline: 0;
-  padding-left    : 18px;
-  padding-right: 30px;
-  ;
-
-  ;
-  position: relative;
-
-
-  text-align: left !important;
-  -webkit-transition: all .2s ease-in-out;
-  transition: all .2s ease-in-out;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-
-
-  white-space: nowrap;
-  width: auto;
-`);
+convertToObject();
 
 module.exports = convertToObject;
